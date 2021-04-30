00126c6636c658dd77d06bf57309b7008411aec4
haijinsha <dengshenkk@gmail.com>
2021-04-30 14:08:50 +0800

    feat: 连接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DATABASE } from '@config.json';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: DATABASE.HOST,
+      port: DATABASE.PORT,
+      username: DATABASE.USERNAME,
+      password: DATABASE.PASSWORD,
+      database: DATABASE.DATABASE,
+      synchronize: true,
+      autoLoadEntities: true,
+      logging: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
